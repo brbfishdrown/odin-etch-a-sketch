5a64271897b659d1b04c2cf41e7ff54b2faf8eb1
seanaelius <dc.seanaelius@gmail.com>
2023-06-20 16:05:57 -0400

    Add pseudocode describing list of features and functionality of website

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //Incorporate pseudocode
+//Create a grid of 16x16 square divs into grid-container
+//Make divs appear as a grid( versus just one on each line )
+//Set up hover effect so that grid divs change colour when mouse passes over them
+//Add a button at the top of the screen prompting the user for the number of squares per side (maximum 100)
+//Be able to choose which colour
+//Have eraser feature
+//Have clear feature
+//Have random colour feature
+//Have progressive darkening feature
