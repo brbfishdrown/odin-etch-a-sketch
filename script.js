752a969857d35370525a4794de091d1615c2f96e
seanaelius <dc.seanaelius@gmail.com>
2023-06-20 16:21:20 -0400

    Define side length Create for-loop that creates appropriate amount of divs for side length Note: Need to style with flex to display 8x8 grid

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-//Incorporate pseudocode
+//FIRST TASK:
 //Create a grid of 16x16 square divs into grid-container
+let side = 16;
+//Define grid-container
+const gridContainer = document.querySelector('.grid-container')
+for (let i = 0; i < side * side; i++) {
+    const grid = document.createElement('div');
+    grid.style.cssText = 'border: 2px solid black; padding: 10px;'
+    gridContainer.appendChild(grid)
+}
+
+
+//TO DO LIST:
+//==============================================================================================================
 //Make divs appear as a grid( versus just one on each line )
 //Set up hover effect so that grid divs change colour when mouse passes over them
 //Add a button at the top of the screen prompting the user for the number of squares per side (maximum 100)
