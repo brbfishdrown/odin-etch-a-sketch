06051de8ca4dc1d990f371d6f46026ad9c8a4002
seanaelius <dc.seanaelius@gmail.com>
2023-06-20 17:03:05 -0400

    Set up functioning grid Implement hover effect to each grid element

--- script.js
+++ script.js
@@ -3,10 +3,18 @@
 let side = 16;
 //Define grid-container
 const gridContainer = document.querySelector('.grid-container')
-for (let i = 0; i < side * side; i++) {
-    const grid = document.createElement('div');
-    grid.style.cssText = 'border: 2px solid black; padding: 10px;'
-    gridContainer.appendChild(grid)
+//Define row; Create (SIDE) number of rows inside container
+for (let i = 0; i < side; i++) {
+    const row = document.createElement('div')
+    row.style.cssText = 'display:flex;'
+    gridContainer.appendChild(row)
+    //Define grid; Create (SIDE) number of grids inside row
+    for (let i = 0; i < side; i++) {
+        const grid = document.createElement('div');
+        grid.style.cssText = 'border: 2px solid black; padding: 10px; /*PADDING FOR VISIBILITY*/'
+        grid.addEventListener('mouseover', function () { grid.style.backgroundColor = 'red' })
+        row.appendChild(grid)
+    }
 }
 
 
